7d0e88d4b5c48fac51fe54bedc8261500cf868cc
martian422 <445971099@qq.com>
2023-06-20 18:03:47 +0800

    modified

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
-修改自：[BIT-srun-login-script](https://github.com/coffeehat/BIT-srun-login-script)，修改了查询IP的正则表达式以在国科大校园网使用，测试时间2022年3月10日。
+修改自：[BIT-srun-login-script](https://github.com/coffeehat/BIT-srun-login-script)，修改了查询IP的正则表达式以在UCAS校园网使用。
 
 
 # 文件说明
-下线脚本似乎会踢掉所有在线设备，不管了将就着用。
+使用如下命令登录校园网：
+
+```bash
+python login.py -u username -p password
+```
+
+警告：logout脚本可能会将所有在线设备强制下线！
 
 |文件|说明|
 |:-:|:-:|
 |BitSrunLogin/|深澜登录函数包|
 |login.py|登录示例脚本|
 |logout.py|下线示例脚本|
-|always_online.py|在线监测脚本，如果监测到掉线则自动重连|
 
-always_online.py可采用`nohup`命令挂在后台：
-``` bash
-nohup python always_online.py &
-```
+
--- logout.py
+++ logout.py
@@ -1,7 +1,47 @@
 from UCASSrunLogin.LoginManager import LoginManager
 
-lm = LoginManager()
-lm.logout(
-    username = "username",
-    password = "password"
-)
+# -*- coding: UTF-8 -*-
+
+
+import sys
+import getopt
+
+
+def main(argv):
+
+    _username_ = ""
+    _password_ = ""
+
+    try:
+        opts, args = getopt.getopt(argv, "hu:p:", ["help", "username=", "password="])
+    except getopt.GetoptError:
+        print('Error: test_arg.py -u <username> -p <password>')
+        print('   or: test_arg.py --username=<username> --password=<password>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print('test_arg.py -u <username> -p <password>')
+            print('or: test_arg.py --username=<username> --password=<password>')
+            sys.exit()
+        elif opt in ("-u", "--username"):
+            _username_ = arg
+        elif opt in ("-p", "--password"):
+            _password_ = arg
+    print('username:', _username_)
+
+    for i in range(0, len(args)):
+        print('parameter %s is %s' % (i + 1, args[i]))
+
+    lm = LoginManager()
+    lm.logout(
+        username = _username_,
+        password = _password_
+    )
+
+
+if __name__ == "__main__":
+
+    main(sys.argv[1:])
+
+
